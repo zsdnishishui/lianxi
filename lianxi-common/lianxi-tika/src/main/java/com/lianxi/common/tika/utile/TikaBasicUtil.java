4e73e213cc405d3bbb351c40701853fb66d0250b
周圣道 <zsdnishishui@163.com>
2023-04-24 17:17:07 +0800

    集成tika文本提取

---
+++ lianxi-common/lianxi-tika/src/main/java/com/lianxi/common/tika/utile/TikaBasicUtil.java
@@ -0,0 +1,50 @@
+package com.lianxi.common.tika.utile;
+
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.sax.BodyContentHandler;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class TikaBasicUtil {
+    public static String fileToTxt(InputStream is) {
+        //1、创建一个parser
+        Parser parser = new AutoDetectParser();
+        try {
+            Metadata metadata = new Metadata();
+            ContentHandler handler = new BodyContentHandler();
+            ParseContext context = new ParseContext();
+            context.set(Parser.class, parser);
+            //2、执行parser的parse()方法。
+            parser.parse(is, handler, metadata, context);
+            for (String name : metadata.names()) {
+                System.out.println(name + ":" + metadata.get(name));
+            }
+            return handler.toString();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (TikaException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (is != null) {
+                    is.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+}
