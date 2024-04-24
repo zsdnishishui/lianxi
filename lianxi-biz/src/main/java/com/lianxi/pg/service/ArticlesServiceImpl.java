4b7db23907a2f6bc271d589b9a367cb9e764c0ff
周圣道 <zsdnishishui@163.com>
2024-04-24 15:18:18 +0800

    整合seata

--- lianxi-biz/src/main/java/com/lianxi/pg/service/ArticlesServiceImpl.java
+++ lianxi-biz/src/main/java/com/lianxi/pg/service/ArticlesServiceImpl.java
@@ -3,6 +3,7 @@ package com.lianxi.pg.service;
 
 import com.lianxi.pg.entity.Articles;
 import com.lianxi.pg.mapper.ArticlesMapper;
+import io.seata.spring.annotation.GlobalTransactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +19,7 @@ public class ArticlesServiceImpl implements ArticlesService {
     }
 
     @Override
+    @GlobalTransactional
     public int add(Articles articles) {
         return studentMapper.add(articles);
     }
