2fe1ca8e7b46fb9e76e1b5bcc95211e115d8e1a2
周圣道 <zsdnishishui@163.com>
2024-04-24 11:25:56 +0800

    整合nacos

--- lianxi-biz/src/main/java/com/lianxi/pg/BizApplication.java
+++ lianxi-biz/src/main/java/com/lianxi/pg/BizApplication.java
@@ -2,10 +2,12 @@ package com.lianxi.pg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan(value = {"com.lianxi"})
 public class BizApplication {
 
