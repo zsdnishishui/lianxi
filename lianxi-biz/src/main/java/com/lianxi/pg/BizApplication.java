20c8ad22bc07072d493b60b03cc0a54e2347ad89
周圣道 <zsdnishishui@163.com>
2024-04-25 13:08:55 +0800

    简化一些配置

--- lianxi-biz/src/main/java/com/lianxi/pg/BizApplication.java
+++ lianxi-biz/src/main/java/com/lianxi/pg/BizApplication.java
@@ -3,14 +3,10 @@ package com.lianxi.pg;
 import io.seata.spring.annotation.datasource.EnableAutoDataSourceProxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 
 
-@SpringBootApplication
-@EnableDiscoveryClient
-@ComponentScan(value = {"com.lianxi"})
+@SpringBootApplication(scanBasePackages = {"com.lianxi"})
 @EnableFeignClients
 @EnableAutoDataSourceProxy
 public class BizApplication {
--- lianxi-biz/src/main/java/com/lianxi/pg/feignClient/RemoteServiceClient.java
+++ lianxi-biz/src/main/java/com/lianxi/pg/feignClient/RemoteServiceClient.java
@@ -6,11 +6,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "lianxi-biz2")
+@FeignClient(name = "lianxi-biz2", path = "/Articles")
 public interface RemoteServiceClient {
-    @GetMapping("/Articles/test/{id}")
+    @GetMapping("/test/{id}")
     R<Articles> test(@PathVariable("id") Integer id);
 
-    @GetMapping("/Articles/delete/{id}")
+    @GetMapping("/delete/{id}")
     R<Integer> delete(@PathVariable("id") Integer id);
 }
